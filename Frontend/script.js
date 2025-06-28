66a8e50dde5bf39846780f71ae17adf62fcfe53f
pagalmanxe <rishavbhattarai2@gmail.com>
2025-06-28 22:07:56 +0545

    repeat code hatako

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,12 +1,14 @@
 
+// tyo css matrai bhanda yo ramro lagyo dropdown ko lagi
 function toggleMenu() {
       document.getElementById('menu').classList.toggle('active');
     }
+    // ad banda garne cross button
   function closeAd() {
       document.getElementById("adPopup").style.display = "none";
     }
 
-    // Auto show popup on page load
+    // page load hune bitekai popup aauxa
     window.onload = function() {
       document.getElementById("adPopup").style.display = "flex";
     };
