7b00e22f0f80e2393b72f4f6e7079003a2f7e4a6
pagalmanxe <rishavbhattarai2@gmail.com>
2025-07-01 14:18:14 +0545

    holyfkairball

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,12 +3,26 @@
 function toggleMenu() {
       document.getElementById('menu').classList.toggle('active');
     }
-    // ad banda garne cross button
+   
   function closeAd() {
       document.getElementById("adPopup").style.display = "none";
     }
 
-    // page load hune bitekai popup aauxa
+   
     window.onload = function() {
       document.getElementById("adPopup").style.display = "flex";
     };
+var slideIndex = 0;
+
+function carousel() {
+  var i;
+  var x = document.querySelectorAll("#slider img"); 
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) { slideIndex = 1; }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000);
+}
+carousel();
