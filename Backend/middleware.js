4dadbcffe75327705b8c6434174ec05f2c85d216
BhugolGautam222 <bhugol.gautam1@gmail.com>
2025-07-02 08:39:35 +0545

    Backend progress

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -1,7 +1,9 @@
 const express= require('express');
 const app= express();
 app.use(express.json());
+const { dbconnect } = require('./dbconnect');
 
-let check= (req,res,)=>{
-
+let check= async(req,res,next)=>{
+    let db =await dbconnect();
+    let coll=db.collection('students');
 }
