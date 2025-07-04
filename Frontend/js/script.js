ebb1733264d411cd13847d2a27c1f745ef301877
pagalmanxe <rishavbhattarai2@gmail.com>
2025-07-04 21:58:57 +0545

    up arrow

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -455,3 +455,21 @@ if (principalCard) {
     );
     observer.observe(principalCard);
 }
+   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    // Show the button when the user scrolls down 100px
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        scrollToTopBtn.style.display = "flex";
+      } else {
+        scrollToTopBtn.style.display = "none";
+      }
+    });
+
+    // Scroll to top smoothly
+    scrollToTopBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
